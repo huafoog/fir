72f54ae13a182dcced6ad77f1d82304513cdd1c4
zyx <123456789>
2022-06-24 14:04:22 +0800

    feat:project init

---
+++ fir-common/fir-common-core/src/main/java/com/zyaxin/fir/common/core/constant/AuthServerConstant.java
@@ -0,0 +1,7 @@
+package com.zyaxin.fir.common.core.constant;
+
+public class AuthServerConstant {
+
+    public static final String SMS_CODE_CACHE_PREFIX="cms:code:";
+    public static final String LOGIN_USER = "MEMBER_USER_INFO";
+}
---
+++ fir-common/fir-common-core/src/main/java/com/zyaxin/fir/common/core/constant/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.zyaxin.fir.common.core.constant;
+
+public class CommonConstants {
+    public final static Integer SUCCESS = 0;
+    public final static Integer FAIL = -1;
+
+
+}
